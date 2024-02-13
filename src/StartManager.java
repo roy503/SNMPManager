1277aea0d2c7c4b672f29110b590de9c516e48f1
Rob <48541302+roy503@users.noreply.github.com>
2024-02-14 10:31:29 +1100

    Delete src directory

--- src/StartManager.java
+++
@@ -1,6 +0,0 @@
-
-public class StartManager {
-	public static void main(String[] args) {
-		new Thread(new SNMPManager()).start();
-	}
-}
--- src/main/java/monitor/Start.java
+++
@@ -1,8 +0,0 @@
-package monitor;
-
-public class Start {
-
-	public static void main(String[] args) {
-		new Thread(new Monitor()).run();
-	}
-}
